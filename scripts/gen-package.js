87cba3795deb76e6cc9749f9f650707f81ee07d5
jacmkno <jose@activisual.net>
2023-05-18 07:34:41 +0000

    tmp

--- scripts/gen-package.js
+++ scripts/gen-package.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const srcs = {
     'pwa/icons': fs.readdirSync("./pwa/icons"),
     'OS.windows': fs.readdirSync("./fonts").map(f=>'fonts/'+f),
+    app: fs.readdirSync("./app"),
     cards: fs.readdirSync("./cards"),
     audios: fs.readdirSync("./audios"),
     timestamp: new Date().getTime()
