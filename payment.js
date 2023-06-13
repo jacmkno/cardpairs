e204a9d589c10fd06e09efc71953b669d9fe3c97
jacmkno <jose@activisual.net>
2023-06-13 08:48:32 -0500

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,3 +1,17 @@
 /*
 Integrate payment processing to enable access levels. call functions from session.js
+
+// Generate payment button
+
+// Generate payment popup to choose a plan
+
+// Call backend for signature. Choose between fake backend and real backend according to settings set by environment variable on deployment.
+
+// Identify client by localstorage key
+
+// Check client access level with backend
+
+// Show payment status / confirmation popup when comming back from payment platform
+
+// Submit payment info to backend for updating status and checking signature before showing payment status.
 */
