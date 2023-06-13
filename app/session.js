0d948ef11d41ad3128c857e0dabef55818a079b6
jacmkno <jose@activisual.net>
2023-06-13 08:51:04 -0500

    Update session.js

--- app/session.js
+++ app/session.js
@@ -1,3 +1,9 @@
 /*
   Determine access level, enable /disable features according to access level.
+  
+  // Identify client with a key in localStorage
+
+  // Check access level from localstorage and fallback to backend
+  
+  // enable / disable features according to access level
 */
